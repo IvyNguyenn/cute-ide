b61b3e3c8820ec18f22d218311e230e4e6e8fd6e
Vy Nguyen <vy.nguyen@logixtek.com>
2023-04-28 11:09:12 +0700

    + Update UI + Add std input and test

--- app/ide/(components)/CodeMirror.tsx
+++ app/ide/(components)/CodeMirror.tsx
@@ -27,8 +27,9 @@ int main(void) {
     message: null,
     status: { id: 0, description: '' },
   });
+  const [error, setError] = useState(undefined);
 
-  const _onChange = (value: string, _viewUpdate: ViewUpdate) => {
+  const _handleChangeCode = (value: string, _viewUpdate: ViewUpdate) => {
     setCode(value);
   };
 
@@ -47,12 +48,21 @@ int main(void) {
       },
     };
     const res = await axios.request(options);
+    setError(undefined);
     setResult({ ...res.data });
   };
 
-  const _createSubmission = async () => {
+  const _createSubmission = async (event: React.SyntheticEvent) => {
     try {
+      event.preventDefault();
       setIsSubmitting(true);
+      const target = event.target as typeof event.target & {
+        language: { value: string };
+        input: { value: string };
+      };
+      const language = target.language.value;
+      const input = target.input.value;
+
       const options = {
         method: 'POST',
         url: `${COMPILER_API_URL}/submissions`,
@@ -66,9 +76,9 @@ int main(void) {
           'X-RapidAPI-Host': RapidAPIHost,
         },
         data: {
-          language_id: 52,
+          language_id: parseInt(language),
           source_code: window.btoa(code),
-          stdin: '',
+          stdin: window.btoa(input),
         },
       };
       const res = await axios.request(options);
@@ -76,8 +86,10 @@ int main(void) {
       if (token) {
         await _getSubmission(token);
       }
-    } catch (err) {
+    } catch (err: any) {
       console.log(err);
+      const errorMessage = JSON.stringify(err?.response?.data) || err?.message;
+      setError(errorMessage);
     } finally {
       setIsSubmitting(false);
     }
@@ -91,53 +103,112 @@ int main(void) {
   return (
     <div className={styles.editor}>
       <div className={styles.editor__code}>
-        <div className={styles.editor__code__controls}>
-          <span>
-            <small>Language: </small>
-            <select defaultValue={52} style={{ flexShrink: 0 }}>
-              <option value={52}>C</option>
-              {/* <option value={63}>JavaScript</option> */}
-            </select>
-          </span>
-          <button
-            type="button"
-            disabled={isSubmitting}
-            onClick={_createSubmission}
-          >
+        <form
+          id="controls-form"
+          className={styles.editor__code__controls}
+          onSubmit={_createSubmission}
+        >
+          <button type="submit" disabled={isSubmitting}>
             Run code
           </button>
-        </div>
+          <span>
+            <small>Language: </small>
+            <select
+              name="language"
+              defaultValue={programingLanguages[3].id}
+              style={{ flexShrink: 0 }}
+              disabled={isSubmitting}
+            >
+              {programingLanguages.map((language) => (
+                <option key={language.id} value={language.id}>
+                  {language.name}
+                </option>
+              ))}
+            </select>
+          </span>
+        </form>
         <CodeMirror
           value={code}
           height="92vh"
           theme={mode}
           extensions={[javascript({ jsx: true })]}
-          onChange={_onChange}
+          onChange={_handleChangeCode}
         />
       </div>
-      <div className={styles.editor__output}>
-        <div>
-          <span style={{ paddingRight: 10 }}>Output:</span>
-          {isSubmitting ? (
-            <small style={{ color: 'var(--blue)' }}>Running code...</small>
-          ) : (
-            <b style={{ color: 'var(--green)' }}>{result.status.description}</b>
-          )}
+      <div className={styles.editor__console}>
+        <div className={styles.editor__console__output}>
+          <div>
+            <span style={{ paddingRight: 10 }}>Output:</span>
+            {isSubmitting ? (
+              <small style={{ color: 'var(--blue)' }}>Running code...</small>
+            ) : (
+              <b style={{ color: 'var(--green)' }}>
+                {result.status.description}
+              </b>
+            )}
+          </div>
+          <div className={styles.editor__console__output__result}>
+            {!isSubmitting && (
+              <>
+                {result.time && (
+                  <p style={{ color: 'var(--blue)' }}>
+                    Finished in {result.time} ms
+                  </p>
+                )}
+                {result.stdout && <p>{window.atob(result.stdout)}</p>}
+                {result.stderr && (
+                  <p style={{ color: 'var(--red)' }}>
+                    {window.atob(result.stderr)}
+                  </p>
+                )}
+              </>
+            )}
+            {error && <p style={{ color: 'var(--red)' }}>{error}</p>}
+          </div>
         </div>
-        <div className={styles.editor__output__result}>
-          {result.time && !isSubmitting && (
-            <>
-              <p style={{ color: 'var(--green)' }}>
-                Finished in {result.time} ms
-              </p>
-              <p>{window.atob(result.stdout)}</p>
-            </>
-          )}
+        <div className={styles.editor__console__input}>
+          <label>Input:</label>
+          <textarea form="controls-form" name="input"></textarea>
         </div>
       </div>
     </div>
   );
 }
+
+const programingLanguages = [
+  { id: 45, name: 'Assembly (NASM 2.14.02)' },
+  { id: 46, name: 'Bash (5.0.0)' },
+  { id: 47, name: 'Basic (FBC 1.07.1)' },
+  { id: 48, name: 'C (GCC 7.4.0)' },
+  { id: 52, name: 'C++ (GCC 7.4.0)' },
+  { id: 49, name: 'C (GCC 8.3.0)' },
+  { id: 53, name: 'C++ (GCC 8.3.0)' },
+  { id: 50, name: 'C (GCC 9.2.0)' },
+  { id: 54, name: 'C++ (GCC 9.2.0)' },
+  { id: 51, name: 'C# (Mono 6.6.0.161)' },
+  { id: 55, name: 'Common Lisp (SBCL 2.0.0)' },
+  { id: 56, name: 'D (DMD 2.089.1)' },
+  { id: 57, name: 'Elixir (1.9.4)' },
+  { id: 58, name: 'Erlang (OTP 22.2)' },
+  { id: 44, name: 'Executable' },
+  { id: 59, name: 'Fortran (GFortran 9.2.0)' },
+  { id: 60, name: 'Go (1.13.5)' },
+  { id: 61, name: 'Haskell (GHC 8.8.1)' },
+  { id: 62, name: 'Java (OpenJDK 13.0.1)' },
+  { id: 63, name: 'JavaScript (Node.js 12.14.0)' },
+  { id: 64, name: 'Lua (5.3.5)' },
+  { id: 65, name: 'OCaml (4.09.0)' },
+  { id: 66, name: 'Octave (5.1.0)' },
+  { id: 67, name: 'Pascal (FPC 3.0.4)' },
+  { id: 68, name: 'PHP (7.4.1)' },
+  { id: 43, name: 'Plain Text' },
+  { id: 69, name: 'Prolog (GNU Prolog 1.4.5)' },
+  { id: 70, name: 'Python (2.7.17)' },
+  { id: 71, name: 'Python (3.8.1)' },
+  { id: 72, name: 'Ruby (2.7.0)' },
+  { id: 73, name: 'Rust (1.40.0)' },
+  { id: 74, name: 'TypeScript (3.7.4)' },
+];
 
 interface SubmissionResult {
   stdout: string;
